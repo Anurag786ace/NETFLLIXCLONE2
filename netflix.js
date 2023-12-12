e02c88ccf5cdad83affdcecce67c48cd6c3edfc1
Anurag Giri <135094302+Anurag786ace@users.noreply.github.com>
2023-12-12 12:34:24 +0530

    Add files via upload

---
+++ netflix.js
@@ -0,0 +1,36 @@
+
+// Add this JavaScript code to your existing script or in a separate file
+
+/*document.addEventListener('DOMContentLoaded', function () {
+    const faqItems = document.querySelectorAll('.faq-item');
+
+    faqItems.forEach(function (item) {
+        const toggleIcon = item.querySelector('.toggle-icon');
+        const answer = item.querySelector('.answer');
+
+        item.addEventListener('click', function () {
+            // Toggle the 'show' class on the answer element
+            answer.classList.toggle('show');
+
+            // Change the text of the toggle icon based on visibility
+            toggleIcon.textContent = answer.classList.contains('show') ? '-' : '+';
+        });
+    });
+});
+
+// Add this JavaScript code to your existing script or in a separate file
+
+document.addEventListener('DOMContentLoaded', function () {
+    const faqItems = document.querySelectorAll('.faq-item');
+
+    faqItems.forEach(function (item) {
+        const toggleIcon = item.querySelector('.toggle-icon');
+        const answer = item.querySelector('.answer');
+
+        item.addEventListener('click', function () {
+            // Toggle the 'show-answer' class on the FAQ item
+            item.classList.toggle('show-answer');
+        });
+    });
+});*/
+
